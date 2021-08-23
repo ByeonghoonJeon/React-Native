7fd15e19c6d60b95d476c8c13d4aa17a261621a3
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-22 22:01:09 -0400

    Practice

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,10 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 const DirectoryNavigator = createStackNavigator(
-  { Directory: { screen: Directory }, CampsiteInfo: { screen: CampsiteInfo } },
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
   {
     initialRouteName: "Directory",
     defaultNavigationOptions: {
@@ -25,32 +28,15 @@ const DirectoryNavigator = createStackNavigator(
 const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      selectedCampsite: null,
-    };
-  }
-
-  onCampsiteSelect(campsiteId) {
-    this.setState({ selectedCampsite: campsiteId });
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Directory
-          campsites={this.state.campsites}
-          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              (campsite) => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+        }}
+      >
+        <AppNavigator />
       </View>
     );
   }
