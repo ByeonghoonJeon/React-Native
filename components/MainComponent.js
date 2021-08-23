33be03f5e9fbc50d6bce5cbcde9eaca6ef5fda23
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-22 20:53:35 -0400

    Practice

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { View } from "react-native";
+import Constants from "expo-constants";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+const DirectoryNavigator = createStackNavigator(
+  { Directory: { screen: Directory }, CampsiteInfo: { screen: CampsiteInfo } },
+  {
+    initialRouteName: "Directory",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
   constructor(props) {
